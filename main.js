7b60916d271ab4825ad63312f2d5060ee4e702df
nderkar <derkar.nilesh@gmail.com>
2020-08-13 16:21:35 +0530

    1308

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+
+         navigator.serviceWorker
+
+                   .register('./sw.js');
+
+     }
+
+}
